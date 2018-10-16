a58e9c7f9a5027514f1b212ed95f7447e7cb41bd
jeantsai <jeantsai@qq.com>
2018-10-16 15:45:38 +0800

    Force update the version of NodeJS Alpine

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push jeantsai/phoenix-alpine
+docker tag jeantsai/phoenix-alpine jeantsai/phoenix-alpine:1.0
+docker push jeantsai/phoenix-alpine:1.0
 
